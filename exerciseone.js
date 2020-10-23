e10578c25f218e53c83c49fdc254df5487052d61
fedeorlandi4@gmail.com <fedeorlandi4@gmail.com>
2020-10-23 19:59:26 -0300

    first exercise done

---
+++ exerciseone.js
@@ -0,0 +1,16 @@
+/*
+Exercise 1:
+Variables y Operadores:
+    1) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+    2) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+    3) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado la suma una 3er variable (utilizar length).
+*/
+
+var numberone, numbertwo, total, name, surname, fullname, day, month, number ;
+total = numberone + numbertwo;
+name = 'Federico';
+surname = 'Orlandi';
+fullname = name + ' ' + surname;
+day = 'Monday';
+month = 'March';
+number = day.length + month.length;
