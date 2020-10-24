e97fd5f0622023188404707fe955c553a6f3e09b
fedeorlandi4@gmail.com <fedeorlandi4@gmail.com>
2020-10-24 12:03:52 -0300

    exercise four done

--- exerciseone.js
+++ exerciseone.js
@@ -167,3 +167,33 @@ if (Age < 2){
     alert('Anciano');
 }
 
+/*
+For
+    Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for de JavaScript para mostrar una alerta utilizando cada una de las palabras.
+    Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra modificada.
+    Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a) recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence. Al final mostrar una única alerta con la cadena completa.
+    Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número de la repetición, es decir que al final de la ejecución del bucle for deberia haber 10 elementos dentro del array, desde el número 0 hasta al numero 9. Mostrar por la consola del navegador el al array final (utilizar console.log).
+*/
+//number one
+var i, mySecondArray;
+mySecondArray = ['chorizo', 'morcilla', 'vacio', 'matambre', 'costilla'];
+for (var i=0; i <=4; i++){
+    alert(mySecondArray[i]);
+}
+//number two
+for (var i=0; i <=4; i++){
+    alert(mySecondArray[i].substring(0,1).toUpperCase() + mySecondArray[i].substring(1).toLowerCase());
+}
+//number three
+var sentence;
+sentence = " ";
+for (var i=0; i <=4; i++){
+    sentence += mySecondArray[i] + " ";
+}
+alert(sentence);
+// number four
+var myThirdArray = [];
+for (var i=0; i <=9; i++){
+    myThirdArray[i] = i;
+}
+console.log(myThirdArray);
